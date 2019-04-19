d578307498f691b1cf7c5bce98ae90d68eb8cdf5
Charandas Batra <charandas@luckydayapp.com>
2019-04-19 15:21:15 -0400

    Add back source

---
+++ packer-post-processor-vsphere-template/plugin.go
@@ -0,0 +1,542 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/hashicorp/packer/helper/config"
+	"github.com/hashicorp/packer/packer"
+	"github.com/hashicorp/packer/template/interpolate"
+	"github.com/vmware/govmomi/find"
+	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/ovf"
+	"github.com/vmware/govmomi/session"
+	"github.com/vmware/govmomi/vim25"
+	"github.com/vmware/govmomi/vim25/progress"
+	"github.com/vmware/govmomi/vim25/soap"
+	"github.com/vmware/govmomi/vim25/types"
+)
+
+var builtins = map[string]string{
+	"mitchellh.virtualbox": "virtualbox",
+	"mitchellh.vmware":     "vmware",
+	"mitchellh.vmware-esx": "vmware",
+}
+
+var parentOfItemRe = regexp.MustCompile(`(?s)<rasd:Parent>(\d+)<\/rasd:Parent>`)
+var networkSectionRe = regexp.MustCompile(`(?s)<NetworkSection>.*<\/NetworkSection>`)
+var virtualboxRe = regexp.MustCompile(`<vssd:VirtualSystemType>virtualbox-(\d)+(\.(\d)+)?<\/vssd:VirtualSystemType>`)
+var virtualboxOSRe = regexp.MustCompile(`<OperatingSystemSection ovf:id="(\d)+">`)
+var virtualboxItemRe = regexp.MustCompile(`(?s)<Item>(.*?)<\/Item>`)
+var virtualboxVboxRe = regexp.MustCompile(`(?s)<vbox:Machine.*<\/vbox:Machine>`)
+
+type VAppPropertyMetadata struct {
+	UserConfigurable bool    `mapstructure:"user_configurable"`
+	Type             string  `mapstructure:"type"`
+	Key              string  `mapstructure:"key"`
+	Value            string  `mapstructure:"value"`
+}
+
+type VAppProperty struct {
+	Metadata         VAppPropertyMetadata `mapstructure:"metadata"`
+	Label            string               `mapstructure:"label"`
+	Description      string               `mapstructure:"description"`
+}
+
+type OVF struct {
+	Datacenter      string `mapstructure:"datacenter"`
+	Datastore       string `mapstructure:"datastore"`
+	Host            string `mapstructure:"host"`
+	Password        string `mapstructure:"password"`
+	Username        string `mapstructure:"username"`
+	Folder          string `mapstructure:"folder"`
+	ResourcePool    string `mapstructure:"resource_pool"`
+	VmName          string `mapstructure:"vm_name"`
+	OsType          string `mapstructure:"os_type"`
+	OsVersion       string `mapstructure:"os_version"`
+	OsID            string `mapstructure:"os_id"`
+	HardwareVersion string `mapstructure:"hardware_version"`
+	NetworkName     string `mapstructure:"network_name"`
+	VAppProperties  []VAppProperty `mapstructure:"v_app_properties"`
+
+	ui       packer.Ui
+	client   *vim25.Client
+	soap     *soap.Client
+	path     string
+	contents []byte
+	// NOTE: we change the contents, envelope still represents originally parsed envelope
+	envelope *ovf.Envelope
+
+	ctx interpolate.Context
+}
+
+func diskParentIndex(content []byte) (p[] byte) {
+	matches := virtualboxItemRe.FindAll(content, -1)
+	allSubmatches := [][]byte{}
+	for _, match := range matches {
+		if bytes.Contains(match, []byte("<rasd:ResourceType>17</rasd:ResourceType>")) {
+			allSubmatches = parentOfItemRe.FindSubmatch(match)
+			break
+		}
+	}
+	return allSubmatches[1]
+}
+
+func (o *OVF) networkMap(finder *find.Finder) (p []types.OvfNetworkMapping){
+	if net, err := finder.Network(context.TODO(), o.NetworkName); err == nil {
+		p = append(p, types.OvfNetworkMapping{
+			Name:    o.NetworkName,
+			Network: net.Reference(),
+		})
+	}
+	return
+}
+
+
+func (o *OVF) normalizeOVF(content []byte) {
+	newHardwareSectionOpenBase := []byte(`
+	<VirtualHardwareSection ovf:transport="com.vmware.guestInfo">
+	`)
+	newHardwareSectionBase := []byte(`
+		<vmw:Config ovf:required="false" vmw:key="tools.afterPowerOn" vmw:value="true"/>
+		<vmw:Config ovf:required="false" vmw:key="tools.afterResume" vmw:value="true"/>
+		<vmw:Config ovf:required="false" vmw:key="tools.beforeGuestShutdown" vmw:value="true"/>
+		<vmw:Config ovf:required="false" vmw:key="tools.beforeGuestStandby" vmw:value="true"/>
+		<vmw:Config ovf:required="false" vmw:key="firmware" vmw:value="bios"/>
+	</VirtualHardwareSection>
+	`)
+
+	newProductSectionBase := []byte(`
+		</OperatingSystemSection>
+		<ProductSection ovf:required="false">
+      <Info>CoreOS Virtual Appliance</Info>
+	`)
+
+	for _, vAppProperty := range o.VAppProperties {
+		metadata := vAppProperty.Metadata
+		newProductSectionBase = append(newProductSectionBase, fmt.Sprintf(`
+			<Property ovf:userConfigurable="%t" ovf:type="%s"
+				ovf:key="%s" ovf:value="%s">`, metadata.UserConfigurable, metadata.Type, metadata.Key, metadata.Value)...)
+		newProductSectionBase = append(newProductSectionBase, fmt.Sprintf(`
+			<Label>%s</Label>
+		`, vAppProperty.Label)...)
+		newProductSectionBase = append(newProductSectionBase, fmt.Sprintf(`
+			<Description>%s</Description>
+		`, vAppProperty.Description)...)
+		newProductSectionBase = append(newProductSectionBase, "</Property>"...)
+	}
+	newProductSectionBase = append(newProductSectionBase, "</ProductSection>"...)
+
+	content = bytes.Replace(content, []byte("<VirtualHardwareSection>"), newHardwareSectionOpenBase, -1)
+	content = bytes.Replace(content, []byte("</VirtualHardwareSection>"), newHardwareSectionBase, -1)
+	content = bytes.Replace(content, []byte("</OperatingSystemSection>"), newProductSectionBase, -1)
+
+	// * Post-processor failed: Line 17: Unsupported element 'Info'
+	content = networkSectionRe.ReplaceAll(content, []byte(fmt.Sprintf(`
+		<NetworkSection>
+			<Info>The list of logical networks</Info>
+			<Network ovf:name="%s">
+				<Description>The VM Network network</Description>
+			</Network>
+		</NetworkSection>
+	`, o.NetworkName)))
+	newNetworkCardItem := fmt.Sprintf(`
+		<Item>
+			<rasd:AddressOnParent>7</rasd:AddressOnParent>
+			<rasd:AutomaticAllocation>true</rasd:AutomaticAllocation>
+			<rasd:Connection>%s</rasd:Connection>
+			<rasd:Description>VmxNet3 ethernet adapter</rasd:Description>
+			<rasd:ElementName>Ethernet 1</rasd:ElementName>
+			<rasd:InstanceID>11</rasd:InstanceID>
+			<rasd:ResourceSubType>VmxNet3</rasd:ResourceSubType>
+			<rasd:ResourceType>10</rasd:ResourceType>
+			<vmw:Config ovf:required="false" vmw:key="wakeOnLanEnabled" vmw:value="true"/>
+		</Item>
+	`, o.NetworkName)
+
+	content = virtualboxItemRe.ReplaceAllFunc(content, func(match []byte) []byte {
+		fmt.Println(string(match))
+		diskParentInstance :=  fmt.Sprintf("<rasd:InstanceID>%s</rasd:InstanceID>", diskParentIndex(content))
+		if bytes.Contains(match, []byte("<rasd:Connection>NAT</rasd:Connection>")) {
+			return []byte(newNetworkCardItem)
+		} else if (bytes.Contains(match, []byte("<rasd:ResourceSubType>PIIX4</rasd:ResourceSubType>")) && bytes.
+				Contains(match, []byte(diskParentInstance))) {
+			// Replace the IDE controller the disk refers to with SCSI.
+			return []byte(fmt.Sprintf(`
+				<Item>
+					<rasd:Address>0</rasd:Address>
+					<rasd:Description>SCSI Controller</rasd:Description>
+					<rasd:ElementName>SCSI Controller 0</rasd:ElementName>
+					%s
+					<rasd:ResourceSubType>VirtualSCSI</rasd:ResourceSubType>
+					<rasd:ResourceType>6</rasd:ResourceType>
+			</Item>
+			`, diskParentInstance))
+		}
+		return match
+	})
+
+	content = virtualboxVboxRe.ReplaceAll(content, []byte(""))
+	// make some of this stuff configurable
+	var newOSSection string
+	if o.OsVersion == "" {
+		newOSSection = fmt.Sprintf(`<OperatingSystemSection ovf:id="%s" vmw:osType="%s">`, o.OsID, o.OsType)
+	} else {
+		newOSSection = fmt.Sprintf(`<OperatingSystemSection ovf:id="%s" ovf:version="%s" vmw:osType="%s">`, o.OsID, o.OsVersion, o.OsType)
+	}
+	content = virtualboxOSRe.ReplaceAll(content, []byte(newOSSection))
+	newHardwareSection := fmt.Sprintf("<vssd:VirtualSystemType>%s</vssd:VirtualSystemType>", o.HardwareVersion)
+	o.contents = virtualboxRe.ReplaceAll(content, []byte(newHardwareSection))
+}
+
+func (o *OVF) initializeClient() error {
+	url := fmt.Sprintf("https://%s:%s@%s/sdk", url.QueryEscape(o.Username), url.QueryEscape(o.Password), o.Host)
+	log.Printf("Connecting to vsphere host with connection string %s", url)
+
+	ctx := context.TODO()
+	// Set up client
+	parsedURL, err := soap.ParseURL(url)
+	if err != nil {
+		return err
+	}
+	soapClient := soap.NewClient(parsedURL, true)
+	client, err := vim25.NewClient(ctx, soapClient)
+	if err != nil {
+		return err
+	}
+	manager := session.NewManager(client)
+	err = manager.Login(ctx, parsedURL.User)
+	if err != nil {
+		return err
+	}
+
+	o.client = client
+	o.soap = soapClient
+	return nil
+}
+
+func (o *OVF) parseOVF() error {
+	content, err := ioutil.ReadFile(o.path)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Open(o.path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	info, err := ovf.Unmarshal(file)
+	if err != nil {
+		return err
+	}
+	if info == nil {
+		return fmt.Errorf("OVF at: %s has an empty envelope", o.path)
+	}
+
+	// replace all virtualbox hardware references so that the OVF file works
+	o.normalizeOVF(content)
+	o.envelope = info
+	return nil
+}
+
+func (o *OVF) uploadItem(lease *object.HttpNfcLease, ovfItem uploadItem) error {
+	item := ovfItem.item
+	filePath := item.Path
+
+	if filePath != o.path {
+		filePath = filepath.Join(filepath.Dir(o.path), filePath)
+	}
+
+	o.ui.Message(fmt.Sprintf("Uploading file: '%s'", filePath))
+
+	log.Printf("Stating file: '%s'", filePath)
+	stat, err := os.Stat(filePath)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Opening file: '%s'", filePath)
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	size := stat.Size()
+
+	// logger := cmd.ProgressLogger(fmt.Sprintf("Uploading %s... ", path.Base(file)))
+	// defer logger.Wait()
+
+	opts := soap.Upload{
+		ContentLength: size,
+		Progress:      ovfItem,
+	}
+
+	// Non-disk files (such as .iso) use the PUT method.
+	// Overwrite: t header is also required in this case (ovftool does the same)
+	if item.Create {
+		opts.Method = "PUT"
+		opts.Headers = map[string]string{
+			"Overwrite": "t",
+		}
+	} else {
+		opts.Method = "POST"
+		opts.Type = "application/x-vnd.vmware-streamVmdk"
+	}
+
+	log.Printf("Uploading file: '%s' (%d) to '%s' with '%s' method", filePath, size, ovfItem.url.String(), opts.Method)
+
+	return o.soap.Upload(file, ovfItem.url, &opts)
+}
+
+func (o *OVF) upload() (*types.ManagedObjectReference, error) {
+	ctx := context.TODO()
+
+	name := "Packer uploaded VMDK"
+	if o.envelope.VirtualSystem != nil {
+		name = o.envelope.VirtualSystem.ID
+		if o.envelope.VirtualSystem.Name != nil {
+			name = *o.envelope.VirtualSystem.Name
+		}
+	}
+
+	if o.VmName != "" {
+		name = o.VmName
+	}
+
+	log.Printf("Configuring import location for '%s'", name)
+
+	finder := find.NewFinder(o.client, true)
+
+	log.Printf("Setting finder datacenter to '%s'", o.Datacenter)
+	datacenter, err := finder.Datacenter(ctx, o.Datacenter)
+	if err != nil {
+		return nil, err
+	}
+	finder.SetDatacenter(datacenter)
+
+	networkMapping := o.networkMap(finder)
+	specParams := types.OvfCreateImportSpecParams{
+		DiskProvisioning: "thin",
+		EntityName:       name,
+		NetworkMapping:   networkMapping,
+	}
+
+	log.Printf("Finding datastore '%s'", o.Datastore)
+	datastore, err := finder.Datastore(ctx, o.Datastore)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Finding folder '%s'", o.Folder)
+	folder, err := finder.Folder(ctx, fmt.Sprintf("/%s/vm/%s", o.Datacenter, o.Folder))
+	if err != nil {
+		return nil, err
+	}
+
+	var resourcePool *object.ResourcePool
+
+	if o.ResourcePool != "" {
+		log.Printf("Finding resource pool '%s'", fmt.Sprintf("/%s/host/%s/Resources", o.Datacenter, o.ResourcePool))
+		resourcePool, err = finder.ResourcePool(ctx, o.ResourcePool)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		log.Print("Using default resource pool")
+		resourcePool, err = finder.DefaultResourcePool(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	manager := object.NewOvfManager(o.client)
+
+	log.Printf("Creating ovf import spec for '%s'", o.path)
+	spec, err := manager.CreateImportSpec(ctx, string(o.contents), resourcePool, datastore, specParams)
+	if err != nil {
+		return nil, err
+	}
+	if spec.Error != nil {
+		return nil, errors.New(spec.Error[0].LocalizedMessage)
+	}
+	if spec.Warning != nil {
+		for _, w := range spec.Warning {
+			log.Printf("Warning: %s\n", w.LocalizedMessage)
+		}
+	}
+
+	log.Print("Creating import lease for VM")
+	lease, err := resourcePool.ImportVApp(ctx, spec.ImportSpec, folder, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	info, err := lease.Wait(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Build slice of items and URLs first, so that the lease updater can know
+	// about every item that needs to be uploaded, and thereby infer progress.
+	var items []uploadItem
+
+	for _, device := range info.DeviceUrl {
+		for _, item := range spec.FileItem {
+			if device.ImportKey != item.DeviceId {
+				continue
+			}
+
+			url, err := o.client.ParseURL(device.Url)
+			if err != nil {
+				return nil, err
+			}
+
+			log.Printf("Creating upload item from '%s' to '%s'", item.Path, url.String())
+			item := uploadItem{
+				url:  url,
+				item: item,
+				ch:   make(chan progress.Report),
+			}
+
+			items = append(items, item)
+		}
+	}
+
+	updater := NewUploaderProgress(o.client, lease, items)
+	defer updater.Done()
+
+	for _, item := range items {
+		err = o.uploadItem(lease, item)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &info.Entity, lease.HttpNfcLeaseComplete(ctx)
+}
+
+func (o *OVF) HandleOVF(ui packer.Ui, ovfPath string) error {
+	o.ui = ui
+	o.path = ovfPath
+
+	err := o.initializeClient()
+	if err != nil {
+		return err
+	}
+	err = o.parseOVF()
+	if err != nil {
+		return err
+	}
+
+	machine, err := o.upload()
+	if err != nil {
+		return err
+	}
+
+	vm := object.NewVirtualMachine(o.client, *machine)
+	vm.MarkAsTemplate(context.TODO())
+
+	return nil
+}
+
+type PostProcessor struct {
+	config OVF
+}
+
+func (p *PostProcessor) Configure(raws ...interface{}) error {
+	err := config.Decode(&p.config, &config.DecodeOpts{
+		Interpolate: true,
+		InterpolateFilter: &interpolate.RenderFilter{
+			Exclude: []string{},
+		},
+	}, raws...)
+
+	if err != nil {
+		return err
+	}
+
+	if p.config.OsType == "" {
+		p.config.OsType = "centos64Guest"
+	}
+
+	if p.config.OsID == "" {
+		p.config.OsID = "107"
+	}
+
+	if p.config.HardwareVersion == "" {
+		p.config.HardwareVersion = "vmx-10"
+	}
+
+	if p.config.Folder == "" {
+		p.config.Folder = "/"
+	}
+
+	if p.config.NetworkName == "" {
+		p.config.NetworkName = "NAT"
+	}
+
+	// Accumulate any errors
+	errs := new(packer.MultiError)
+
+	templates := map[string]*string{
+		"datacenter": &p.config.Datacenter,
+		"host":       &p.config.Host,
+		"password":   &p.config.Password,
+		"username":   &p.config.Username,
+		"datastore":  &p.config.Datastore,
+	}
+
+	for key, ptr := range templates {
+		if *ptr == "" {
+			errs = packer.MultiErrorAppend(
+				errs, fmt.Errorf("%s must be set", key))
+		}
+	}
+
+	if len(errs.Errors) > 0 {
+		return errs
+	}
+
+	return nil
+}
+
+func (p *PostProcessor) PostProcess(ui packer.Ui, artifact packer.Artifact) (packer.Artifact, bool, error) {
+	if _, ok := builtins[artifact.BuilderId()]; !ok {
+		return nil, false, fmt.Errorf("Unknown artifact type, can't build box: %s", artifact.BuilderId())
+	}
+
+	ovfPath := ""
+	vmdkPath := ""
+	for _, path := range artifact.Files() {
+		if strings.HasSuffix(path, ".ovf") {
+			ovfPath = path
+			break
+		} else if strings.HasSuffix(path, ".vmdk") {
+			vmdkPath = path
+		}
+	}
+
+	if ovfPath == "" || vmdkPath == "" {
+		return nil, false, fmt.Errorf("ERROR: OVF/VMDK were found")
+	}
+
+	// only pass the ovf, the vmdk should be referenced in it
+	err := p.config.HandleOVF(ui, ovfPath)
+	if err != nil {
+		return nil, false, err
+	}
+
+	return artifact, false, nil
+}
